c88a7864bd13387e3138ed381e2833f275fa7db8
Alex <alex@example.com>
2024-07-07 10:30:48 +0300

    array

--- 6Seniors/Learn JS/5/5.5 array metods day 3 practice/main.js
+++ 6Seniors/Learn JS/5/5.5 array metods day 3 practice/main.js
@@ -8,18 +8,33 @@
 // camelize("list-style-image") == 'listStyleImage';
 // camelize("-webkit-transition") == 'WebkitTransition';
 
-function camelize(str) {
-    let step1 = str.split('-')
-    step2 = step1.map(function(index, word){
-        if(index == 0){
-           let a = word.charAt(0).toUpperCase() 
-           return a
-            
+// function camelize(str){
+//     let step1 = str.split("")
+//     let newString = ""
+//     for(let i = 0; i < step1.length; i++){
+//        if(step1[i] === "-"){
+//         let findElement = step1[i + 1];
+//         upperElement = step1
+//        }
+//     }
+    
+// }
+
+function camelize(str){
+    let step1 = str.split("");
+    let newString = "";
+    for(let i = 0; i < step1.length; i++){
+        if(step1[i] === "-" && i < step1.length - 1){
+            let upperAfter = step1[i + 1].toUpperCase();
+            newString += upperAfter;
+            i++;
+        } else {
+            newString += step1[i]
         }
-        
-    })
-    return step2
-} 
+    }
+    return newString
+}
 
-
-console.log(camelize('-webkit-transition'));
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
---
+++ 6Seniors/Learn JS/5/5.5 array metods day 4/main.js
@@ -0,0 +1,152 @@
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+// Примеры:
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+
+
+// function camelize(str) {
+//     return str.split('-') .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join(''); 
+//   }
+
+// console.log(camelize("-webkit-transition"));
+
+
+
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет элементы со значениями больше или равными a и меньше или равными b и возвращает результат в виде массива.
+
+// Функция должна возвращать новый массив и не изменять исходный.
+
+// Например:
+
+// let arr = [5, 3, 8, 1];
+
+// let filtered = filterRange(arr, 1, 4);
+
+// alert( filtered ); // 3,1 (совпадающие значения)
+
+// alert( arr ); // 5,3,8,1 (без изменений)
+
+
+// function filterRange(arr, a, b){
+//     let newArr = []
+//     arr.map(function(item){
+//         if (item >= a && item <= b){
+//             newArr.push(item)
+//         }
+//     }
+//   )
+//   return newArr;
+// }
+
+// let arr = [2, 2, 3, 1, 5, 8];
+
+// let filtered = filterRange(arr, 1, 4)
+// console.log(filtered);
+// console.log(arr);
+
+
+
+// function filterRangeInPlace(arr, a, b){
+//     for (let i = 0; i < arr.length; i++){
+//         let del = arr[i]; 
+
+//         if(del < a || del > b){
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//     }
+    
+
+
+// }
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4);
+// console.log(arr);
+
+
+
+
+
+
+// let arr = [5, 2, 1, -10, 8];
+
+// ... ваш код для сортировки по убыванию
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+
+// function numeric(a, b){
+//     return b - a
+// }
+
+// let arr = [5, 2, 1, -10, 8];
+// console.log(arr.sort(numeric)); // [8, 5, 2, 1, -10]
+
+
+
+
+
+
+
+
+// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+
+// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// function copySorted(arr){
+//     let step1 = arr.concat()
+//     let step2 = step1.sort()
+//     return step2
+// }
+
+// let sorted = copySorted(arr);
+// console.log(sorted);
+// console.log(arr);
+
+// sorted[1] = 123;
+// console.log(sorted);
+// console.log(arr);
+
+
+
+
+function Calculator(){
+    this.calculate = function(str){
+        let [a, op, b] = str.split(" ");
+        let num1 = parseFloat(a);
+        let num2 = parseFloat(b);
+
+
+        switch(op){
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            default:
+                throw new Error("Неподдерживаемый оператор")
+        }
+    }
+
+    
+}
+
+
+let calc = new Calculator();
+console.log(calc.calculate("1 + 2"));
+console.log(calc.calculate("5 - 7"));

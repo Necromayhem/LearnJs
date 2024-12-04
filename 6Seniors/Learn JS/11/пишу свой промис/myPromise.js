925698e6f6a2b0fc37c1c3bd0868e420a5bee5ba
Alex <aleksanderkuklev1488@gmail.com>
2024-12-04 15:49:53 +0300

    my promise

--- 6Seniors/Learn JS/11/пишу свой промис/myPromise.js
+++ 6Seniors/Learn JS/11/пишу свой промис/myPromise.js
@@ -1,1 +1,130 @@
-alert(1488)
+// my Promise:
+
+const FULFILLED = 'fulfilled';
+const PENDING = 'pending';
+const REJECTED = 'rejected';
+
+class myPromise {
+    constructor(executor){
+         this.state = PENDING;
+         this.result = undefined;
+         this.onFulfiledFn = [];
+         this.onRejectedFn = [];
+        
+        const resolve = (value) =>{
+            if(this.state === PENDING){
+                this.state = FULFILLED;
+                this.result = value;
+                this.onFulfiledFn.forEach((fn) =>
+                    fn(value));
+            }
+        };
+
+        const reject = (error) => {
+            if(this.state === PENDING){
+                this.state = REJECTED;
+                this.result = error; 
+                this.onRejectedFn.forEach((fn) =>
+                    fn(error));
+            }
+        };
+
+        try {
+            executor(resolve, reject)
+        } catch(error){
+            reject(error); 
+        }
+        
+    }
+
+    then = (onFulfiled, onRejected) => {
+        if(this.state === PENDING){
+            if(onFulfiled){
+                this.onFulfiledFn.push(onFulfiled)
+            }
+            if(onRejected){
+                this.onRejectedFn.push(onRejected)
+            }
+        }
+
+        if(onFulfiled && this.state === FULFILLED){
+            onFulfiled(this.result)
+            return;
+        }
+
+        if(onRejected && this.state === REJECTED){
+            onRejected(this.error)
+            return;
+        }
+    }
+
+    catch = (onRejected) => {
+        this.then(null, onRejected)
+    }
+}
+
+// const promise = new myPromise((resolve, reject) => {
+//     reject('error')
+// })
+
+// console.log(promise);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

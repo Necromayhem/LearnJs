d166cfe7d997e3b70fdf223c4d1216d20c5fe569
Alex <alex@example.com>
2025-01-24 18:27:35 +0300

    1

---
+++ think/questions_vue/vue-test/src/components/v-html.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('<em>Изначальный текст<em>')
+
+const btn = () => {
+  text.value == '<strong>Новый текст<strong>'
+    ? (text.value = '<em>Изначальный текст<em>')
+    : (text.value = '<strong>Новый текст<strong>')
+}
+</script>
+
+<template>
+  <span v-html="text"></span>
+  <div @click="btn"><button>Изменить текст</button></div>
+</template>
